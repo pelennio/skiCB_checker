d25924de8b1ce422f6f90ad99b523200e996e6a6
Olena <pelennio@gmail.com>
2023-11-09 12:19:29 -0600

    refactor of publisher

---
+++ src/publisher.js
@@ -0,0 +1,96 @@
+// @ts-check
+const csv = require("@fast-csv/parse");
+const moment = require("moment");
+const { writeToPath } = require("fast-csv");
+/**
+ * @param {string} promo the name of Promo, can be BWE, ES or Standard
+ * @param {string} subtotal full price per trip without discount
+ * @param {string} taxesFees taxes and fees
+ * @param {string} onlineTotal full price per trip with online discount
+ * @param {string} rewardsTotal full price per trip with REWORDS discount (-20%)
+ */
+export async function publish(
+  promo,
+  subtotal,
+  taxesFees,
+  onlineTotal,
+  rewardsTotal
+) {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const myDate = moment().format("MM-D-YYYY");
+  const day = weekday[moment().weekday()];
+
+  async function writeTheResults() {
+    let myList = new Promise((resolve) => {
+      let myObj = [];
+      csv
+        .parseFile("curent_prices/price.csv", { headers: true })
+        .on("data", (data) => {
+          myObj.push(data);
+        })
+        .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`))
+        .on("finish", () => {
+          resolve(myObj);
+        });
+    });
+
+    let output = await myList;
+    let changeVector = "";
+    await output.push({
+      Date: `${myDate}`,
+      DayOfWeek: `${day}`,
+      Package: `${promo}`,
+      Subtotal: `${subtotal}`,
+      "Taxes&Fees": `${taxesFees}`,
+      OnlineTotal: `${onlineTotal}`,
+      RewardsTotal: `${rewardsTotal}`,
+      ChangeVector: changeVector,
+    });
+
+    const lastPriceData =
+      output[Object.keys(output)[Object.keys(output).length - 1]];
+    const lastTotalPrice = Number(lastPriceData.RewardsTotal.split("$")[1]);
+
+    /**
+     * @returns {Number} returns pre-last price
+     */
+    const preLastTotalPrice = () => {
+      for (let i = Object.keys(output).length - 2; i > 0; i--) {
+        if (output[Object.keys(output)[i]].Package == `${promo}`) {
+          const preLastTotalPrice = Number(
+            output[Object.keys(output)[i]].RewardsTotal.split("$")[1]
+          );
+          return preLastTotalPrice;
+        }
+      }
+      return 0;
+    };
+    const diff = Math.round(lastTotalPrice - preLastTotalPrice());
+    if (diff > 0) {
+      console.log(`${promo}:*** 😤 The price is increasing by😤 ***`, diff);
+      changeVector = `⬆︎ (${diff})`;
+    } else if (diff == 0) {
+      console.log(`${promo}:*** 💅🏻 The price is stable 💅🏻 ***`);
+      changeVector = "🟰";
+    } else {
+      console.log(`${promo}:*** 👀 The price is dropping by: 👀 ***`, -diff);
+      changeVector = `⬇︎ (${diff})`;
+    }
+    output[Object.keys(output)[Object.keys(output).length - 1]].ChangeVector =
+      changeVector;
+    console.log("New values added to the list: *******\n", lastPriceData);
+
+    ///WRITING results to the .csv file
+    writeToPath("curent_prices/price.csv", output, { headers: true });
+  }
+  await writeTheResults();
+  return;
+}
--- tests/skicb_price_checker.spec.js
+++ tests/skicb_price_checker.spec.js
@@ -1,100 +1,13 @@
 // @ts-check
 const { test } = require("@playwright/test");
 const moment = require("moment");
-const csv = require("@fast-csv/parse");
-const { writeToPath } = require("fast-csv");
 import { Components } from "../components";
+import { publish } from "../src/publisher";
 
 test.describe("Price check: ", async () => {
-  const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  const checkInDate = "12/19/2023";
+  const checkOutDate = "12/26/2023";
   const myDate = moment().format("MM-D-YYYY");
-  const day = weekday[moment().weekday()];
-
-  /**
-   * @param {string} promo the name of Promo, can be BWE, ES or Standard
-   * @param {string} subtotal full price per trip without discount
-   * @param {string} taxesFees taxes and fees
-   * @param {string} onlineTotal full price per trip with online discount
-   * @param {string} rewardsTotal full price per trip with REWORDS discount (-20%0
-   * @param {string} changeVector demonstrate price change vector comparing to last price
-   */
-  async function writeTheResults(
-    promo,
-    subtotal,
-    taxesFees,
-    onlineTotal,
-    rewardsTotal
-  ) {
-    let myList = new Promise((resolve) => {
-      let myObj = [];
-      csv
-        .parseFile("curent_prices/price.csv", { headers: true })
-        .on("data", (data) => {
-          myObj.push(data);
-        })
-        .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`))
-        .on("finish", () => {
-          resolve(myObj);
-        });
-    });
-
-    let output = await myList;
-    let changeVector = "";
-    await output.push({
-      Date: `${myDate}`,
-      DayOfWeek: `${day}`,
-      Package: `${promo}`,
-      Subtotal: `${subtotal}`,
-      "Taxes&Fees": `${taxesFees}`,
-      OnlineTotal: `${onlineTotal}`,
-      RewardsTotal: `${rewardsTotal}`,
-      ChangeVector: changeVector,
-    });
-
-    const lastPriceData =
-      output[Object.keys(output)[Object.keys(output).length - 1]];
-    const lastTotalPrice = Number(lastPriceData.RewardsTotal.split("$")[1]);
-
-    /**
-     * @returns {Number} returns pre-last price
-     */
-    const preLastTotalPrice = () => {
-      for (let i = Object.keys(output).length - 2; i > 0; i--) {
-        if (output[Object.keys(output)[i]].Package == `${promo}`) {
-          const preLastTotalPrice = Number(
-            output[Object.keys(output)[i]].RewardsTotal.split("$")[1]
-          );
-          return preLastTotalPrice;
-        }
-      }
-      return 0;
-    };
-    const diff = Math.round(lastTotalPrice - preLastTotalPrice());
-    if (diff > 0) {
-      console.log(`${promo}:*** 😤 The price is increasing by😤 ***`, diff);
-      changeVector = `⬆︎ (${diff})`;
-    } else if (diff == 0) {
-      console.log(`${promo}:*** 💅🏻 The price is stable 💅🏻 ***`);
-      changeVector = "🟰";
-    } else {
-      console.log(`${promo}:*** 👀 The price is dropping by: 👀 ***`, -diff);
-      changeVector = `⬇︎ (${diff})`;
-    }
-    output[Object.keys(output)[Object.keys(output).length - 1]].ChangeVector =
-      changeVector;
-    console.log("New values added to the list: *******\n", lastPriceData);
-
-    ///WRITING results to the .csv file
-    writeToPath("curent_prices/price.csv", output, { headers: true });
-  }
 
   test.beforeEach(async ({ page }) => {
     await page.goto(
@@ -106,9 +19,9 @@ test.describe("Price check: ", async () => {
     page,
   }) => {
     const component = new Components(page);
-    await component.skibd.checkInDate.fill("12/19/2023");
+    await component.skibd.checkInDate.fill(checkInDate);
     await component.skibd.checkOutDate.click();
-    await component.skibd.checkOutDate.fill("12/26/2023");
+    await component.skibd.checkOutDate.fill(checkOutDate);
     await component.skibd.peopleCount.fill("4");
     await component.skibd.searchButton.click();
     await component.skibd.bweOption.click();
@@ -127,13 +40,7 @@ test.describe("Price check: ", async () => {
     );
 
     const pricePromo = "BWE";
-    writeTheResults(
-      `${pricePromo}`,
-      subtotal,
-      taxesFees,
-      onlineTotal,
-      rewardsTotal
-    );
+    publish(`${pricePromo}`, subtotal, taxesFees, onlineTotal, rewardsTotal);
     await page.screenshot({
       path: `curent_prices/${pricePromo}:${myDate}.png`,
     });
@@ -141,9 +48,9 @@ test.describe("Price check: ", async () => {
 
   test("Extended Snowcation", async ({ page }) => {
     const component = new Components(page);
-    await component.skibd.checkInDate.fill("12/19/2023");
+    await component.skibd.checkInDate.fill(checkInDate);
     await component.skibd.checkOutDate.click();
-    await component.skibd.checkOutDate.fill("12/26/2023");
+    await component.skibd.checkOutDate.fill(checkOutDate);
     await component.skibd.peopleCount.fill("4");
     await component.skibd.searchButton.click();
     await component.skibd.esOption.click();
@@ -161,13 +68,7 @@ test.describe("Price check: ", async () => {
       component.skibd.rewardsTotal
     );
     const pricePromo = "ES";
-    writeTheResults(
-      `${pricePromo}`,
-      subtotal,
-      taxesFees,
-      onlineTotal,
-      rewardsTotal
-    );
+    publish(`${pricePromo}`, subtotal, taxesFees, onlineTotal, rewardsTotal);
     await page.screenshot({
       path: `curent_prices/${pricePromo}:${myDate}.png`,
     });
@@ -175,9 +76,9 @@ test.describe("Price check: ", async () => {
 
   test("Standard room option", async ({ page }) => {
     const component = new Components(page);
-    await component.skibd.checkInDate.fill("12/19/2023");
+    await component.skibd.checkInDate.fill(checkInDate);
     await component.skibd.checkOutDate.click();
-    await component.skibd.checkOutDate.fill("12/26/2023");
+    await component.skibd.checkOutDate.fill(checkOutDate);
     await component.skibd.peopleCount.fill("4");
     await component.skibd.searchButton.click();
     await component.skibd.standardOption.click();
@@ -195,13 +96,7 @@ test.describe("Price check: ", async () => {
       component.skibd.rewardsTotal
     );
     const pricePromo = "STANDARD";
-    writeTheResults(
-      `${pricePromo}`,
-      subtotal,
-      taxesFees,
-      onlineTotal,
-      rewardsTotal
-    );
+    publish(`${pricePromo}`, subtotal, taxesFees, onlineTotal, rewardsTotal);
     await page.screenshot({
       path: `curent_prices/${pricePromo}:${myDate}.png`,
     });
