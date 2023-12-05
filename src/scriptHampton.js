3629a01f59267c033380984720072be02cfbdc8f
Olena <pelennio@gmail.com>
2023-12-04 20:39:32 -0600

    12

--- src/scriptHampton.js
+++ src/scriptHampton.js
@@ -1,6 +1,9 @@
 import * as graph from "./graphBuilder.js";
-const any = "2 Queen Room Fridge and Microwave";
+const any1 = "2 Queen Room Fridge and Microwave first";
+const any2 = "2 Queen Room Fridge and Microwave second";
 const csvPath = "../curent_prices/hampton-price.csv";
 
-graph.parseData(csvPath, graph.createGraph, "Honor Price", any, "#chart");
+graph.parseData(csvPath, graph.createGraph, "Honor Price", any1, "#chart");
 graph.lastPrace(csvPath, graph.createGraphSmall, "#chart5");
+
+graph.parseData(csvPath, graph.createGraph, "Honor Price", any2, "#chart1");
--- tests/hampton_checker.spec.js
+++ tests/hampton_checker.spec.js
@@ -6,15 +6,18 @@ import { publish } from "../src/publisher";
 test.describe("Price check: ", async () => {
   const csvPath = "curent_prices/hampton-price.csv";
 
-  test("1-st room option", async ({ page }) => {
+  /**
+   * @param {string} checkInDate
+   * @param {string} checkOutDate
+   */
+  async function checkPrice(checkInDate, checkOutDate, visit, { page }) {
     const component = new Components(page);
     await page.goto(
-      "https://www.hilton.com/en/book/reservation/rooms/?ctyhocn=AMADHHX&arrivalDate=2023-12-18&departureDate=2023-12-19&room1NumAdults=1"
+      `https://www.hilton.com/en/book/reservation/rooms/?ctyhocn=AMADHHX&arrivalDate=${checkInDate}&departureDate=${checkOutDate}&room1NumAdults=2&room1NumChildren=2`
     );
     const roomType = await component.hampton.roomType.innerText();
     await component.hampton.moreRatesButton.click();
     await component.hampton.flexRateHonor.click();
-
     const onlineTotal = await component.hampton.priceDetail(
       component.hampton.totalRoomChargeAmount
     );
@@ -24,7 +27,20 @@ test.describe("Price check: ", async () => {
     const rewardsTotal = await component.hampton.priceDetail(
       component.hampton.totalForStayAmount
     );
+    publish(
+      csvPath,
+      roomType + " " + visit,
+      visit,
+      taxTotal,
+      onlineTotal,
+      rewardsTotal
+    );
+  }
 
-    publish(csvPath, roomType, "n/a", taxTotal, onlineTotal, rewardsTotal);
+  test("1-st room option", async ({ page }) => {
+    await checkPrice("2023-12-18", "2023-12-19", "first", { page });
+  });
+  test("2-st room option", async ({ page }) => {
+    await checkPrice("2023-12-26", "2023-12-27", "second", { page });
   });
 });
